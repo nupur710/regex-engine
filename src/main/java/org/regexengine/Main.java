4bc7b1f2b0f379864d94eb026a376d3f51d7e5d5
Nupur Singh <nupur.singh2410@gmail.com>
2024-07-21 23:58:12 +0530

    prevent infinite epsilon loop

--- src/main/java/org/regexengine/Main.java
+++ src/main/java/org/regexengine/Main.java
@@ -5,19 +5,22 @@ import java.util.List;
 
 public class Main {
 
+    //Have to specify NFA --> the program constructs an NFA engine
+    //use it to test if entered string input matches pattern of NFA
+
     public static void main(String[] args) {
         EngineNFA engineNFA= new EngineNFA();
-        engineNFA.declareStates("q0", "q1", "q2", "q3");
+        engineNFA.declareStates("q0", "q1", "q2");
         engineNFA.setInitialState(engineNFA.getStateObject("q0"));
-        State q3= engineNFA.getStateObject("q3");
+        State q3= engineNFA.getStateObject("q2");
         List<State> finalStatesList= new ArrayList<>();
         finalStatesList.add(q3);
         engineNFA.setFinalStates(finalStatesList);
         engineNFA.addTransition(engineNFA.getStateObject("q0"), engineNFA.getStateObject("q1"), new CharacterMatcher('a'));
+        engineNFA.addTransition(engineNFA.getStateObject("q1"), engineNFA.getStateObject("q1"), new EpsilonMatcher());
         engineNFA.addTransition(engineNFA.getStateObject("q1"), engineNFA.getStateObject("q2"), new CharacterMatcher('b'));
-        engineNFA.addTransition(engineNFA.getStateObject("q2"), engineNFA.getStateObject("q2"), new CharacterMatcher('b'));
-        engineNFA.addTransition(engineNFA.getStateObject("q2"), engineNFA.getStateObject("q3"), new EpsilonMatcher());
-        System.out.println(engineNFA.compute("a"));
+    //    engineNFA.addTransition(engineNFA.getStateObject("q2"), engineNFA.getStateObject("q3"), new EpsilonMatcher());
+        System.out.println(engineNFA.compute("ab"));
 
 
     }
